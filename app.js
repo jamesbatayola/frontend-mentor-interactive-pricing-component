9ace0895827c5b4630658cffad5f874ecaee6c24
Jake James Batayola <batayolajakejames@gmail.com>
2024-01-25 14:19:18 +0800

    added input range animations

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const input = document.querySelector("input");
 
+const switchButton = document.querySelector(".switch");
+
 input.addEventListener("input", () => {
 	const value = input.value;
 	console.log(`The value is ${value}`);
+});
+
+switchButton.addEventListener("click", () => {
+	switchButton.classList.toggle("active");
 });
